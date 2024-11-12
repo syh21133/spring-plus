c9effab0460c57f1c39a836b94d53fe62a3c2c3f
syh213 <syh21133@gmail.com>
2024-11-12 18:09:33 +0900

    refactor : 필수 기능 2단계 -8
    8.QueryDSL
    
    JPQL로 작성된 findByIdWithUser 를 QueryDSL로 변경

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,7 +1,6 @@
 package org.example.expert.domain.todo.repository;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,18 +8,20 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoRepositoryCustom {
 
     @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-        "LEFT JOIN t.user " +
-        "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+//    @Query("SELECT t FROM Todo t " +
+//        "LEFT JOIN t.user " +
+//        "WHERE t.id = :todoId")
+//    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
 
     @Query(" SELECT t FROM Todo t WHERE (:weather IS NULL OR t.weather = :weather) AND (:startDate IS NULL OR t.modifiedAt >= :startDate) AND (:endDate IS NULL OR t.modifiedAt <= :endDate) ORDER BY t.modifiedAt DESC ")
     Page<Todo> findTodoByCondition(@Param("weather") String weather,
         @Param("startDate") LocalDateTime startDate,
         @Param("endDate") LocalDateTime endDate, Pageable pageable);
+
 }
